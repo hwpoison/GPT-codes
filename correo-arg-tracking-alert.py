6a9795f228f5dbda9330de379cc651dd2289f249
Guillermo Giménez <guichezero@gmail.com>
2023-03-16 10:57:39 -0300

    Add files via upload

--- correo-arg-tracking-alert.py
+++ correo-arg-tracking-alert.py
@@ -9,49 +9,42 @@ url = "<URL>"
 # Configurar la instancia de ToastNotifier
 toaster = ToastNotifier()
 
-# Obtener la página web
-response = requests.get(url)
-html = response.content
-
-# Analizar el HTML de la página
-soup = BeautifulSoup(html, 'html.parser')
-
-# Buscar la tabla de seguimiento
-tabla_seguimiento = soup.find('div', {'id': 'resultado'}).find('table')
-
-# Almacenar la tabla previa para compararla con la nueva
-tabla_previa = tabla_seguimiento
+previous_content = None
 
 while True:
-    # Esperar 10 segundos antes de volver a revisar la página
-    time.sleep(10)
+        print("Checking...")
+        # Obtener la página web
+        response = requests.get(url)
+        html = response.content
 
-    # Obtener la página web
-    response = requests.get(url)
-    html = response.content
+        # Analizar el HTML de la página
+        soup = BeautifulSoup(html, 'html.parser')
 
-    # Analizar el HTML de la página
-    soup = BeautifulSoup(html, 'html.parser')
+        # Buscar la tabla de seguimiento
+        tracking_table = soup.find('div', {'id': 'resultado'}).find('table')
 
-    # Buscar la tabla de seguimiento
-    tabla_seguimiento = soup.find('div', {'id': 'resultado'}).find('table')
+        # Obtener todos los elementos <tr> dentro de la tabla
+        fields = tracking_table.find_all('tr')
 
-    # Obtener todos los elementos <tr> dentro de la tabla
-    filas = tabla_seguimiento.find_all('tr')
+        # Seleccionar el último elemento de la lista de fields
+        last_field = fields[1]
 
-    # Seleccionar el último elemento de la lista de filas
-    ultima_fila = filas[1]
+        # Obtener el contenido de cada celda de la última fila
+        fields = last_field.find_all('td')
+        date = fields[0].text.strip()
+        location = fields[1].text.strip()
+        history = fields[2].text.strip()
+        status = fields[3].text.strip()
 
-    # Obtener el contenido de cada celda de la última fila
-    celdas = ultima_fila.find_all('td')
-    fecha = celdas[0].text.strip()
-    planta = celdas[1].text.strip()
-    historia = celdas[2].text.strip()
-    estado = celdas[3].text.strip()
-
-    # Comparar la tabla previa con la nueva
-    if tabla_seguimiento != tabla_previa:
-        print('¡La tabla de seguimiento ha sido actualizada!')
-        # Enviar una alerta o realizar cualquier acción necesaria
-        tabla_previa = tabla_seguimiento
-        toaster.show_toast("Nueva actualización!", f"{ historia } en { planta }", duration=250)
+        # Comparar la tabla previa con la nueva
+        if previous_content is None:
+            previous_content = tracking_table
+        elif tracking_table != previous_content:
+            print('¡La tabla de seguimiento ha sido actualizada!')
+            # Enviar una alerta o realizar cualquier acción necesaria
+            previous_content = tracking_table
+            toaster.show_toast("Nueva actualización!", f"{ history } en { location }", duration=500)
+        else:
+            print("Nothing new for the moment...")
+        # Esperar 30 segundos antes de volver a revisar la página
+        time.sleep(30)
